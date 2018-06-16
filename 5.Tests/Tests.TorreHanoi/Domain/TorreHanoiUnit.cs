d6907c50d87cf409b48a17d87e6c356d14757cdc
alucardri <alucardri@gmail.com>
2018-06-16 15:35:45 -0300

    Correção do retorno da imagem.
    Correção do último movimento da Torre.
    
    Verifiquei algumas outras coisas que poderiam ser alteradas, porém foquei apenas na task que foi passada.

--- 5.Tests/Tests.TorreHanoi/Domain/TorreHanoiUnit.cs
+++ 5.Tests/Tests.TorreHanoi/Domain/TorreHanoiUnit.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Domain.TorreHanoi;
 using Infrastructure.TorreHanoi.Log;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -23,14 +24,49 @@ namespace Tests.TorreHanoi.Domain
         [TestCategory(CategoriaTeste)]
         public void Construtor_Deve_Retornar_Sucesso()
         {
-            Assert.Fail();
+            var torre = new global::Domain.TorreHanoi.TorreHanoi(3, _mockLogger.Object);
+
+            Assert.IsNotNull(torre);
+            Assert.IsNotNull(torre.Id);
+
+            Assert.IsNotNull(torre.Discos);
+            Assert.AreEqual(torre.Discos.Count, 3);
+
+            Assert.IsNotNull(torre.Destino);
+            Assert.AreEqual(torre.Destino.Discos.Count, 0);
+
+            Assert.IsNotNull(torre.Intermediario);
+            Assert.AreEqual(torre.Intermediario.Discos.Count, 0);
+
+            Assert.IsNotNull(torre.Origem);
+            Assert.AreEqual(torre.Origem.Discos.Count, 3);
+
+            Assert.IsTrue(torre.DataCriacao > DateTime.Today);
+            Assert.AreEqual(torre.Status, TipoStatus.Pendente);
+            Assert.IsNotNull(torre.PassoAPasso);
+
+
+            Assert.AreEqual(torre.Origem.Tipo, TipoPino.Origem);
+            Assert.AreEqual(torre.Destino.Tipo, TipoPino.Destino);
+            Assert.AreEqual(torre.Intermediario.Tipo, TipoPino.Intermediario);
         }
 
         [TestMethod]
         [TestCategory(CategoriaTeste)]
-        public void Processar_Deverar_Retornar_Sucesso()
+        public void Processar_Deve_Retornar_Sucesso()
         {
-            Assert.Fail();
+            var quantidadeDiscos = 3;
+            var quantidadePassos = Math.Pow(2, quantidadeDiscos) - 1;
+
+            var torre = new global::Domain.TorreHanoi.TorreHanoi(quantidadeDiscos, _mockLogger.Object);
+
+            torre.Processar();
+
+            Assert.AreEqual(TipoStatus.FinalizadoSucesso, torre.Status);
+            Assert.AreEqual(quantidadeDiscos, torre.Destino.Discos.Count);
+            Assert.AreEqual(0, torre.Origem.Discos.Count);
+            Assert.AreEqual(0, torre.Intermediario.Discos.Count);
+            Assert.AreEqual(quantidadePassos, torre.PassoAPasso.Count);
         }
     }
 }
